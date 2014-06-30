12975485542106c410ace0a93887e60a7fb3218c
jonschlinkert <dev@sellside.com>
2014-06-30 07:36:51 -0400

    use `this.push(file)` instead of in the callback (https://github.com/jonschlinkert/gulp-rte/issues/1#issuecomment-47438377)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var through = require('through2');
 var Route = require('rte');
 var extend = require('xtend');
@@ -8,10 +7,10 @@ module.exports = function (propstring, opts) {
   var rte = new Route(opts);
   rte.set('dest', ':dest/' + (propstring || ':basename:ext'));
 
-  function route(file, enc, cb) {
+  return through.obj(function(file, enc, cb) {
     var options = extend({}, {dest: file.base}, opts);
     file.path = rte.dest(file.path, 'dest', options);
-    cb(null, file);
-  }
-  return through.obj(route);
+    this.push(file);
+    cb();
+  });
 };
